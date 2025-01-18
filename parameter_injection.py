e96a4ce3d881801557a7c7bcdca2cfc8f4349205
linuser <tgjuranec@gmail.com>
2025-01-18 18:50:33 +0100

    Parameter Injection challenge solved!

--- parameter_injection.py
+++ parameter_injection.py
@@ -120,17 +120,82 @@ def decrypt_flag(shared_secret: int, iv: str, ciphertext: str):
     if is_pkcs7_padded(plaintext):
         return unpad(plaintext, 16).decode('ascii')
     else:
-        return plaintext.decode('ascii')
+        return  plaintext.decode('ascii')
     
 
-jsonstring = '{"p": "0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", "g": "0x02", "A": "0xfc48146d42ec9a6be08e403a9a9b971595891a75f8de4d0a843c6a74dbba2c0dc967896febc61e54baa6a99076d8e7a55ae3e2f2ae0abbaa589ce82e27b9122461c0ab2002248f126366d6cc50e5083342028c9ed2e64d6e64b604fd24fd3d28d44667ffd415c2f0637dbd9a4fde0d0b6094f4bf221dfb6abf9fcb4e2e9aa8f1c80aca34242ddf91486e5a64e32bd628e6a6e4ea255bcc77ec0727de4203240c34daf8d98efcb936c8af95836a7d51e82a7cb3931997b9037db504a44b7ce5b1"}'
-jsondata = json.loads(jsonstring)
-p = int(jsondata['p'], 16)
-g = int(jsondata['g'], 16)
-A = int(jsondata['A'], 16)
+import socket
 
-afake = randint(2, p-2)
+HOST = "socket.cryptohack.org"
+PORT = 13371
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect((HOST,PORT))
+
+
+send_string_init = '{"p": "0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", "g": "0x02", "A": "0xd31eb8cf8bc053e138f99c8d488e312b6cb9036699734150abc070cd2489d984a33a6edb7b4996fee67ad960e8b596d4498d4428b54f4f079635513925153ca3c29e3f73cf3323140a623ae049c758ffe0216ba78082950f771eddef6a1b8ef9fbfcd4eb7fc53e70d025c61bb242035a52cfd5ea685e87850929cc37b474fa577ed85c10cc9f7e48582dc209ff3ee8880af148608f2cadfa016681d2882ee4b5815fafa6b985fe150e59d97265a02e3e9a24d2aaa29ecc2035504bf08b8b175d"}'
+# sock.sendall(send_string_init.encode())
+
+# RECEIVED FROM ALICE 
+# jsonstringA = '{"p": "0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", "g": "0x02", "A": "0x3586aed0de267ca203d04fb3998858184ab4e42c3d669723f519b8a0167fe38ea0e062030e888f33f44aedbbd23ecaa12beb731581a11f8288eb3ae61524abab5cfc858d20073b01a201544d08f1e660339f03642c027be6eef1d2cc518b3052bbe7977689c91612d39f57ca9861da206b549f68f77e236cd6213d2cdf09905183a5951cdb66968a1f767ba0e936f3383d81b2f999a45d4c9c873d3ede86c1187e18db3319dbf3fb6a036d37500cd3a741e962359b231c44a0a27f24082f996d"}'
+
+
+input = sock.recv(2048).decode("ascii")
+print(input)
+sock.sendall(send_string_init.encode())
+input = sock.recv(2048).decode("ascii")
+print(input)
+jsonstringA = input[input.find("{"):input.find("}")+1]
+jsondataA = json.loads(jsonstringA)
+p = int(jsondataA['p'], 16)
+g = int(jsondataA['g'], 16)
+A = int(jsondataA['A'], 16)
+# CREATE A FAKE PRIVATE KEY FOR COMMUNICATION WITH ALICE!!!
+bfake = 333930369188680653901414021193501082933468091819283816669584810395166174808302943255462316998695156419180731436157472105378630043018086278586683863706286109613420712607996668419148638810498417062689558428013085174438528226046603670388918919373717415640051551773140685483832440550029001834316040395694753459757921160169976415556998386813135879261018474231015132847802876386310312997724996443102630178291977930488943955806884736516494900888414493843880549405227938
+if(bfake >= p): bfake = p-2
+# print(f'bfake = {bfake}')
+Bfake = pow(g,bfake, p)
+shared_secret_Bfake = pow(A,bfake,p)
+
+jsonstringB = '{"B": "0xb4f0c5c303578957d69ce94b4f4eef9cedd4d648aea6c1ba0bc11a8cd65032e56c4a6f1f0e17c263d0d86cbaa244776a435c4f2aaaacd1b93c23db98b915ccb8002952083e9b11c8e22cd7ca96838d9b24b5a7ab8d8d3b47d27ae14c429508d758d01b018ea53b2a38317a18d364169b5fe8fac88e98205d78b115aeb705b2b4113829d4db2c95bfca66fa53b08cd4ca3fa56096da01b95d053b019391bf837b2725c50757bdae517fed8fcc5507ec4b9e5ea9e3d0bc13731e8e2a25c8577813"}'
+# input = sock.recv(2048).decode("ascii")
+# jsonstringB = input[input.find("{"):input.find("}")+1]
+jsondataB = json.loads(jsonstringB)
+B = int(jsondataB['B'],16)
+jsondataBfake = jsondataB.copy()
+jsondataBfake['B'] = hex(Bfake)
+send_stringALICE= json.dumps(jsondataBfake)
+sock.sendall(send_stringALICE.encode()) 
+
+
+
+# CREATE A FAKE PRIVATE KEY FOR COMMUNICATION WITH BOB!!!
+afake = 83315473272357384846570709455750312386225469842831224928636409015704267958037525939485821431421825479524711877005780064113698383460922919114931098064702502903228347408054855035492687899656552806152380394348194067436194413367252284496170938543304518408263049666989201917305500664843267047065108125234703181158480009562477273093056645398026223021207081954979927382393897970209481340825675244445627700451420913968792083491113709154958397221963754436009983505881762
+if(afake >= p): afake = p-2
 Afake = pow(g, afake, p)
 shared_secret_Afake = pow(A, afake, p)
+jsondataAfake = jsondataA.copy()
+jsondataAfake['A'] = hex(Afake)
+send_stringBOB = json.dumps(jsondataAfake)
+sock.sendall(send_stringALICE.encode())
+
+input = str(sock.recv(2048),"ascii")
+print(input)
+if input.find('encrypted_flag') == -1:
+    sock.sendall(send_stringALICE.encode())
+    input = str(sock.recv(2048),"ascii")
+    print(input)
+# last received string consists of:
+# - Intercepted from Bob: {"B": ...}\n Intercepted from Alice: {"iv": "...", "encrypted_flag": "..."}\n'
+string_result = input[input.rfind("{"):input.rfind("}",)+1]
+jsonResult = json.loads(string_result)
+iv = jsonResult['iv']
+encrypted_flag = jsonResult['encrypted_flag']
+
+print(decrypt_flag(shared_secret_Bfake, iv,encrypted_flag))
+
+
+
+pass
+
 
 
